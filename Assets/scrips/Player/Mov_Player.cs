ae4e65d440aaca7aeff84710a52700bf180bf12b
Alumno <alumno@academic.local>
2025-10-14 16:23:54 -0300

    walala

--- Assets/scrips/Player/Mov_Player.cs
+++ Assets/scrips/Player/Mov_Player.cs
@@ -1,35 +1,48 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class Mov_Player : MonoBehaviour
 {
+    PlayerController player;
+    private CharacterController controller;
+    Camera Camera = Camera.main;
+
     [Header("Valores del Movimiento")]
     private float Speed = 1f;
+    private float speedrun = 2f;
     private float jumpHeight = 1.5f;
     private float GravityValue = -9.81f;
-    private bool suelo_player;
 
     [HideInInspector] private Vector3 velocity;
     [HideInInspector] private Vector2 input;
+    [HideInInspector] private Vector3 current_mouse;
     [HideInInspector] private Vector3 moveDirection;
-
-    [HideInInspector] private CharacterController controller;
+    [HideInInspector] private bool disparando = false;
+    [HideInInspector] private bool saltando = false;
+    [HideInInspector] private bool Agachado = false;
 
 
     private void Awake()
     {
-       controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
     }
     void Start()
     {
-       
     }
 
     void Update()
     {
-        // suelo_player = controller.isGrounded;
-        Vector3 move = new Vector3(input.x, 0, input.y);
-        controller.Move(move*Time.deltaTime);
+        if (controller.isGrounded)
+        {
+            Vector3 move = new Vector3(input.x, 0f, input.y);
+            moveDirection = Vector3.ClampMagnitude(move, Speed);
+
+        }
+        moveDirection.y += GravityValue * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -37,4 +50,25 @@ public class Mov_Player : MonoBehaviour
         input = context.ReadValue<Vector2>();
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //Mouse.current.position.ReadValue()
+        current_mouse = Mouse.current.position.ReadValue();
+        Camera.transform.position  = current_mouse;
+
+    }
+
+    public void Oninteraccion(InputAction.CallbackContext context)
+    {
+
+        
+       //cambiar de armas
+     }
+
+    // direccion de la camara siguiendo mouse
 }
--- Assets/scrips/Player/PlayerController.cs
+++ Assets/scrips/Player/PlayerController.cs
@@ -1,17 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    GameObject arma; 
+    GameObject current_Item;
+    List<GameObject> list;
 
-
-    
     void Start()
     {
-        
+
     }
     void Update()
     {
-        
+
+    }
+
+    void cambio_item()
+    {
+
     }
 }
+   
+
